3fccf310850201cb81bd505bf6856a928ea1f9c4
Jonas Berdal <jonas.berdal.holm@gmail.com>
2023-10-17 11:30:27 +0200

    SydSpiller ferdig

--- src/no/hvl/dat100/prosjekt/kontroll/SydSpiller.java
+++ src/no/hvl/dat100/prosjekt/kontroll/SydSpiller.java
@@ -6,6 +6,7 @@ import no.hvl.dat100.prosjekt.kontroll.spill.Handling;
 import no.hvl.dat100.prosjekt.kontroll.spill.HandlingsType;
 import no.hvl.dat100.prosjekt.kontroll.spill.Spillere;
 import no.hvl.dat100.prosjekt.modell.Kort;
+import no.hvl.dat100.prosjekt.modell.KortSamling;
 
 /**
  * Klasse som for å representere en vriåtter syd-spiller. Strategien er å lete
@@ -39,8 +40,36 @@ public class SydSpiller extends Spiller {
 
 		// TODO - START
 		/* first-fit strategi */
-	
-		throw new UnsupportedOperationException(TODO.method());
+		
+		Kort[] hand = getHand().getAllekort();
+		KortSamling lovlige = new KortSamling();
+		
+		
+		for (Kort k : hand) {
+			if (Regler.kanLeggeNed(k, topp)) {
+				lovlige.leggTil(k);
+			}
+			
+				
+		}
+		
+		Kort spill = null;
+
+		
+		Handling handling = null;
+		if (!lovlige.erTom()) {
+			spill = lovlige.taSiste(); //ta første
+			handling = new Handling(HandlingsType.LEGGNED, spill);
+			
+		}
+		else if (getAntallTrekk() < Regler.maksTrekk()) {
+			handling = new Handling(HandlingsType.TREKK, null);
+		} else {
+			handling = new Handling(HandlingsType.FORBI, null);
+			// setAntallTrekk(0);
+			
+		}
+		return handling;
 	
 		// TODO - END
 	}
